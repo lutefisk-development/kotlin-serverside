597302604263cf26ae504ae44ba338e10543154f
Per Kristian <per-kristian.svanberg@klaravik.se>
2024-01-05 11:24:58 +0100

    Print

---
+++ src/main/kotlin/Print.kt
@@ -0,0 +1,23 @@
+package org.example
+
+fun main() {
+    /**
+     * This is used to print to the console
+     * variation of the print method by overloading.
+     *
+     * Adds a new line after execution
+     *
+     * Can pass strings
+     * Can pass numbers
+     * Can pass objects
+     * Can pass characters
+     */
+    println("Hello from")
+    println("the other side")
+
+    /**
+     * Print everything on onw line
+     */
+    print("Hello from")
+    println("the other side")
+}
