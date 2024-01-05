00134d65ff0a13240ce728b482b0bc36759360fe
Per Kristian <per-kristian.svanberg@klaravik.se>
2024-01-05 09:40:17 +0100

    Reserved keywords

---
+++ src/main/kotlin/ReservedKeywords.kt
@@ -0,0 +1,25 @@
+package org.example
+
+fun main() {
+    /**
+     * var - Declares a mutable property or local variable
+     */
+    var name = "Test Person"
+
+    println(name)
+
+    /**
+     * Other reserved keywords:
+     *
+     * as
+     * as?
+     * break
+     * class
+     * continue
+     * do
+     * else
+     * false
+     * for
+     * fun
+     */
+}
