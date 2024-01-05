25913c8c898c88101a5fbad2169e054711ad084c
Per Kristian <per-kristian.svanberg@klaravik.se>
2024-01-05 11:38:24 +0100

    Varibles and Datatypes

---
+++ src/main/kotlin/Variables_Datatypes.kt
@@ -0,0 +1,35 @@
+package org.example
+
+fun main() {
+    /**
+     * To declare a variable use the keywords:
+     * var
+     * val
+     */
+    var name: String = "Klaravik"
+    var age: Int = 12
+
+    //println(name)
+    //println(age)
+    println("name: ${name} - age: ${age}")
+
+
+    /**
+     * Datatypes:
+     */
+    var n: Int     = 55
+    var l: Long    = 100L
+    var d: Double  = 3.14
+    var f: Float   = 3.14F
+    var b: Boolean = true
+    var s: String  = "Yeay"
+    var c: Char    = 'A'
+
+
+    /**
+     * Any type
+     * Can hold all datatypes
+     */
+     var number2: Any = 10
+    var name2: Any = "Hello"
+}
