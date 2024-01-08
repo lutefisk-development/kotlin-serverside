383b916f8835ab0d2ea58f6769f049a6974771ed
Per Kristian <per-kristian.svanberg@klaravik.se>
2024-01-08 15:00:40 +0100

    Null safety

---
+++ src/main/kotlin/Null_Safety.kt
@@ -0,0 +1,12 @@
+package org.example
+
+fun main() {
+    var name = "Tony"
+    println(name.uppercase())
+
+    /**
+     * Define the type and add a questionmark. The variable can be of the type OR null
+     */
+    var brand: String? = "Volvo"
+    println(brand?.uppercase())
+}
